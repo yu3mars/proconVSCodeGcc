ea59402f33d123f40c78ee9386d7b19fdabbc1b0
yu3mars <yu3mars@users.noreply.github.com>
2018-10-03 21:31:17 +0900

    add sample

---
+++ sample/d.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    printf("Hello2\n");
+    int a = 2;
+    int b = 3;
+    int c;
+    cin >> c;
+    //c = a + b;
+ 
+    printf("c(%d) = a(%d) + b(%d) \n", c, a, b);
+ 
+    return 0;
+}
